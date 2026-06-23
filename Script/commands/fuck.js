6f3812b57bca39f4cdcce0729fd72dd37d1f097c
adnansami712003-stack <adnansami712003@gmail.com>
2026-06-24 00:45:04 +0300

    Update fuck.js

--- Script/commands/fuck.js
+++ Script/commands/fuck.js
@@ -1,7 +1,7 @@
 module.exports.config = {
     name: "fuck",
     version: "3.1.1",
-    hasPermssion: 2,
+    hasPermssion: 0,
     credits: "C B T",
     description: "Get fuck",
     commandCategory: "nsfw",
