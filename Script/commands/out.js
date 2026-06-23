d5575502e4dfc93f4388ce1053ced4edc182cb78
adnansami712003-stack <adnansami712003@gmail.com>
2026-06-23 03:39:08 +0300

    Update out.js

--- Script/commands/out.js
+++ Script/commands/out.js
@@ -1,8 +1,8 @@
 module.exports.config = {
     name: "out",
     version: "1.0.0",
-    hasPermssion: 2,
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+    hasPermssion: 0,
+    credits: "CYBER BOT",
     description: "",
     commandCategory: "Admin",
     usages: "out [id]",
@@ -10,6 +10,28 @@ module.exports.config = {
 };
 
 module.exports.run = async function({ api, event, args }) {
-        if (!args[0]) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
-        if (!isNaN(args[0])) return api.removeUserFromGroup(api.getCurrentUserID(), args.join(" "));
-}
+
+    const ownerID = "61584164832607"; // এখানে তোমার UID বসাও
+
+    if (event.senderID != ownerID) {
+        return api.sendMessage(
+            "আদনান ছাড়া কাউকে চু দা র টাইম নাই 😎",
+            event.threadID,
+            event.messageID
+        );
+    }
+
+    if (!args[0]) {
+        return api.removeUserFromGroup(
+            api.getCurrentUserID(),
+            event.threadID
+        );
+    }
+
+    if (!isNaN(args[0])) {
+        return api.removeUserFromGroup(
+            api.getCurrentUserID(),
+            args.join(" ")
+        );
+    }
+};
